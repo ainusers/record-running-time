0a2ee421065e29ddbb762e29df69e9e53b504a28
ty <979111986@qq.com>
2019-11-20 09:49:25 +0800

    record_running_time method方法record update

--- src/main/java/com/analysis/ainusers/method/Record.java
+++ src/main/java/com/analysis/ainusers/method/Record.java
@@ -1,15 +1,14 @@
 package com.analysis.ainusers.method;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author: tianyong
  * @date: 2019/11/12 13:52
  * @desciption:记录方法运行时间 (自定义注解)
  */
+@Inherited
+@Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Record {
--- src/main/java/com/analysis/ainusers/method/RecordMethod.java
+++ src/main/java/com/analysis/ainusers/method/RecordMethod.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 /**
  * @author: tianyong
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
  */
 @Aspect
 @Component
+@Configuration
 public class RecordMethod {
 
     // 获取目标Logger
